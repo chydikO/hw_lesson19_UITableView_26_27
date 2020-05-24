2f7baf7f6facb1a4daddd96ed678719954991009
Oleg Chudnovskiy MB_Pro_13 <oleg.chudnovskiy@gmail.com>
2020-05-24 11:31:06 +0300

    add PictureController and xib

--- hw_lesson19_UITableView_26_27/UI/Task26/Cells/PictureCell.swift
+++ hw_lesson19_UITableView_26_27/UI/Task26/Cells/PictureCell.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class PictureCell: TableCell {
 
+    @IBOutlet private var picture: UIImageView?
+    @IBOutlet var separated: UIView?
     
-    
+    var data: Picture? {
+            didSet {
+                if let pictureName = data?.name {
+                    let image = UIImage(named: pictureName) ?? UIImage(named: "emptyPicture")
+                    self.picture?.image = image
+                } else {
+                    self.picture? = UIImageView()
+                }
+            }
+        }
+    }
+
+extension PictureCell: SeparatedProtocol {
+    var separatedView: UIView? {
+        return separated
+    }
 }
---
+++ hw_lesson19_UITableView_26_27/UI/Task26/PictureController.swift
@@ -0,0 +1,41 @@
+//
+//  PictureController.swift
+//  hw_lesson19_UITableView_26_27
+//
+//  Created by OlegChudnovskiy on 24.05.2020.
+//  Copyright © 2020 OlegChudnovskiy. All rights reserved.
+//
+
+import UIKit
+
+class PictureController: ViewController {
+    @IBOutlet private var picture: UIImageView?
+
+    private var data: Picture?
+
+    override func viewDidLoad() {
+       super.viewDidLoad()
+        view.backgroundColor = .white
+        self.title = "User Profile"
+        setData()
+    }
+     private func setData() {
+            if let pictureName = self.data?.name {
+                let image = UIImage(named: pictureName) ?? UIImage(named: "emptyPicture")
+                self.picture?.image = image
+            } else {
+                self.picture? = UIImageView()
+            }
+        }
+        
+        //MARK: - init
+        
+        init(picture: Picture) {
+            self.data = picture
+            super.init(nibName: PictureController.className, bundle: nil)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
